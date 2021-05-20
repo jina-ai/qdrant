3d2fa61e7b6364dc3d82d2be54c0251e9d3f3ba6
Joan Fontanals <joan.martinez@jina.ai>
2021-05-20 19:05:57 +0200

    Add text uri content (#11)

--- python_bindings/src/lib.rs
+++ python_bindings/src/lib.rs
@@ -5,6 +5,7 @@ use pyo3::prelude::*;
 /// A Python module implemented in Rust.
 #[pymodule]
 fn qdrant_segment_py(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_class::<qdrant_types::PySearchParams>()?;
     m.add_class::<qdrant_types::PyVectorIndexType>()?;
     m.add_class::<qdrant_types::PyPayloadIndexType>()?;
     m.add_class::<qdrant_types::PyDistanceType>()?;
